623e82b70bfae48335b120871574847482ad1ba2
kubanscripts <130844381+kubanscripts@users.noreply.github.com>
2024-06-24 05:34:34 +1000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,36 +1,49 @@
 Config = {}
 
-Config.StartLocation = {
-    coords = vector3(91.77, -1604.23, 29.9)
+Config.Framework = 'qbcore' -- Options: 'qbcore', 'esx'
+Config.InteractionType = 'target' -- Options: 'target', 'drawmarker'
+Config.TextUIType = 'drawtext' -- Options: 'oxlib', 'drawtext'
+Config.DiscordWebhook = 'YOUR_WEBHOOK_URL'
+
+Config.Notify = 'ox' -- Options: 'qb', 'ox'
+Config.Inventory = 'ox' -- Options: 'qb', 'ox'
+
+Config.CityHall = {
+    Pos = vector3(-264.58, -964.87, 31.22),
+    Marker = {
+        Type = 27, -- https://docs.fivem.net/docs/game-references/markers/
+        Size = vector3(1.0, 1.0, 1.0),
+        Color = {r = 0, g = 255, b = 0, a = 100}
+    },
+    Target = {
+        Size = 2,
+        PedModel = 'a_m_y_business_01', -- https://docs.fivem.net/docs/game-references/ped-models/
+        PedPos = vector3(-264.67, -964.84, 30.22),
+        PedHeading = 204.07
+    }
+}
+Config.TextUI = '[~g~E~w~] City Hall'
+Config.WeaponLicenseItem = 'weaponlicense'
+Config.HuntingLicenseItem = 'huntinglicense'
+
+Config.Jobs = {
+    { name = 'Police', job = 'police', description = 'Join the police force.', icon = 'fas fa-shield-alt' },
+    { name = 'EMS', job = 'ambulance', description = 'Join the emergency medical services.', icon = 'fas fa-ambulance' },
+    { name = 'Mechanic', job = 'mechanic', description = 'Become a mechanic.', icon = 'fas fa-wrench' },
 }
 
-Config.DeliveryVan = {
-    model = "rumpo",
-    spawnCoords = vector4(98.67, -1603.6, 29.59, 229.59)
+Config.Licenses = {
+    { name = 'Weapon License', type = 'weapon', description = 'Obtain a weapon license.', icon = 'fas fa-id-card' },
+    { name = 'Hunting License', type = 'hunting', description = 'Obtain a hunting license.', icon = 'fas fa-paw' },
 }
 
-Config.DeliveryLocations = {
-    { coords = vector4(-1409.23, -636.29, 27.67, 211.09), name = 'Bahama Mamas', distance = '' },
-    { coords = vector4(519.21, 169.05, 98.37, 247.8), name = 'Vinewood Hills', distance = '' }
-}
-
-Config.PayAmount = { min = 400, max = 600 }
-
-Config.Marker = { type = 27, color = {r = 0, g = 255, b = 0}, size = { x = 2.0, y = 2.0, z = 2.0 } }
-
-Config.FinishLocation = {
-    coords = vector3(93.88, -1613.73, 28.52)
-}
-
-Config.Texts = {
-    startDeliveryPrompt = "[~g~E~w~] Start Delivery",
-    deliveryPrompt = "[~g~E~w~] Deliver the boxes",
-    finishDeliveryPrompt = "[~g~E~w~] Finish Delivery",
-    alreadyOnDelivery = "You are already on a delivery!",
-    driveToDelivery = "Drive to the delivery location!",
-    deliveringBoxes = "Delivering the boxes...",
-    driveBackToFinish = "Drive back to the finish location to complete the delivery.",
-    deliveryCancelled = "Delivery cancelled.",
-    deliveryCompleted = "Delivery completed! You've been paid $",
-    somethingWentWrong = "Something went wrong, try again later."
+Config.Laws = {
+    WeaponLaws = {
+        'No carrying of illegal weapons',
+        'No discharging firearms in public',
+    },
+    VehicleLaws = {
+        'No speeding over the limit',
+        'No driving under the influence',
+    }
 }
